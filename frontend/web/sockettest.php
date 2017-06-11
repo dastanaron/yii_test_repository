07795ccee242bd6d1f9fc78596280ee32ac90f5e
dastanaron <flow199@yandex.ru>
2017-06-12 02:12:44 +0600

    Added experimental files for socked connection

---
+++ frontend/web/sockettest.php
@@ -0,0 +1,27 @@
+<?php
+echo "hello world";
+
+?>
+<script>
+    var socket = new WebSocket("ws://127.0.0.1:8000");
+    socket.onopen = function() {
+        console.log("Соединение установлено.");
+    };
+
+    socket.onclose = function(event) {
+        if (event.wasClean) {
+            console.log('Соединение закрыто чисто');
+        } else {
+            console.log('Обрыв соединения'); // например, "убит" процесс сервера
+        }
+        console.log('Код: ' + event.code + ' причина: ' + event.reason);
+    };
+
+    socket.onmessage = function(event) {
+        console.log("Получены данные " + event.data);
+    };
+
+    socket.onerror = function(error) {
+        console.log("Ошибка " + error.message);
+    };
+</script>
